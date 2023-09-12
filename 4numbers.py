1caccefea775ae156ba173f3d2552a7028d06e82
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-12 12:37:24 +0530

    Numbers and it's functions

---
+++ 4numbers.py
@@ -0,0 +1,25 @@
+from math import *  #it imports all library functions
+
+
+
+number = 6
+number2 = 12
+print(20%6) #returns remainder
+
+print('number is ', number)
+
+#change int to string
+number3 = str(number2)
+print("number 2 is " + number3)
+
+#Number functions: built-in
+print(abs(-4))  #returns absolute value of number
+print(max(5, 2)) #returns maximum
+print(min(5, 2)) #return minimum
+print(round(3.4)) # returns round off
+print(bin(3)) #converts number to a binary string
+
+#Imported Library Functions
+
+print(sqrt(100))
+print(pow(3, 5)) 
