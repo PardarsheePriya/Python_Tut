7bc3b88996b2891504adf658ed933b0995923a3f
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-12 12:26:55 +0530

    variables in python

---
+++ 2Variables.py
@@ -0,0 +1,10 @@
+#variable
+
+name = 'Pardarshee'   #strings
+print(name)
+print(name + ' is a girl')  #concat
+
+age = 20  #integer
+print(age)
+print(name + ' is', age)  #only concatenate strings
+
