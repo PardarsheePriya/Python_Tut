89b8f05ed137b52059e7058ce22a993091ca0757
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-12 12:27:28 +0530

    strings

---
+++ 3string.py
@@ -0,0 +1,9 @@
+print("Hi, How are you?")
+name = "Pardarshee"
+print(name)
+
+#String is character array, so we can access each element
+print(name[0])
+
+print(name.islower()) #boolean (it checks whether all string are lower or not)
+print(name.upper().isupper()) #first converted to upper case and then checked 
