65a62d519e53f6ab43dbf3dd9359cbf07759a4ac
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-14 21:00:09 +0530

    for loop

---
+++ 14for_loop.py
@@ -0,0 +1,20 @@
+for letter in "Hello":
+  print(letter)
+
+
+mylist = [1, 2, 4, 21, 53, 123]
+
+for i in mylist:
+  print(i)
+
+mydict = {
+  'name': "Pardarshee",
+  'age': 12,
+  'UID': "20BCS4614"
+}
+
+for values in mydict:
+  print(values)
+
+for x in range(2, 10):  #returns from 2 to 9, excluding 10
+  print(x)
