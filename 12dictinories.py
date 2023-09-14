5555a47904baf854fca73c7f4bdf4e1c8d947922
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-14 20:50:16 +0530

    Dictionary

---
+++ 12dictinories.py
@@ -0,0 +1,14 @@
+my_dict = {
+  'name': 'Pardarshee', 
+  'age' : 21,
+  'nationality': 'Indian',
+  'qualification': 'Bachelor of Engineering in Computer Science'
+}
+
+print(my_dict)
+print(my_dict['name']) #returns value for the given key
+
+#Duplicate keys are not allowed 
+
+print(len(my_dict))
+ 
