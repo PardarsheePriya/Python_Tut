077deed2626881984e9a87287f0a22a7ef96da1a
Pardarshee <pardarsheepriyal@gmail.com>
2023-09-14 20:39:01 +0530

    If-else

---
+++ 11if_statement.py
@@ -0,0 +1,17 @@
+number = int(input("Enter a number: "))
+
+if(number%2 == 0):
+  print("Number is Even")
+
+else:
+  print("Number is Odd5")
+
+a = False
+b = 5
+
+if(a == True):
+  print("A is True")
+elif(a == False):
+  print("A is False")
+else:
+  print("A is neither True nor False")
